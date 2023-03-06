88847329e18660158f33e3ec79a7e6d9e5c82d2e
gardzielb <bartosz.g.1999@gmail.com>
2023-03-06 22:30:13 +0100

    Added script for running prediction in parallel

---
+++ predict_feeding.py
@@ -0,0 +1,36 @@
+import argparse
+import subprocess
+from pathlib import Path
+from datetime import datetime
+
+import pandas as pd
+
+if __name__ == '__main__':
+	arg_parser = argparse.ArgumentParser()
+	arg_parser.add_argument('data_path', type = str)
+	arg_parser.add_argument('-m', '--model-path', type = str)
+	arg_parser.add_argument('-r', '--rec-data', type = str)
+	args = arg_parser.parse_args()
+
+	start_time = datetime.now()
+
+	rec_df = pd.read_csv(args.rec_data)
+	brood_ids = list(rec_df['brood_id'].unique())
+	data_path = Path(args.data_path)
+	brood_dirs = [path for path in data_path.rglob('*') if path.stem in brood_ids]
+
+	processes = []
+	for brood_dir in brood_dirs:
+		cmd = [
+			'./venv/bin/python', '-m', 'beggingcallsanalyzer', 'predict',
+			args.model_path, brood_dir.as_posix(), '--extension', 'WAV'
+		]
+		processes.append(subprocess.Popen(cmd))
+
+	for process in processes:
+		exit_code = process.wait()
+		if exit_code:
+			print(f'Process {process} exited with code {exit_code}')
+
+	task_duration = datetime.now() - start_time
+	print(f'Task completed in {task_duration}')
